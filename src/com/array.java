39c6c05c4dce0b9caa526a88234d7fb88d625d58
dhanushyasubramanian <dhanushyasubramanian20@gmail.com>
2025-07-02 15:00:28 +0530

    class

---
+++ src/com/array.java
@@ -0,0 +1,10 @@
+package src.com;
+
+public class array {
+    public static void main(String[]args){
+        int []marks={89,99,98,95,98};
+        for (int mark:marks){
+            System.out.println(mark);
+        }
+    }
+}
