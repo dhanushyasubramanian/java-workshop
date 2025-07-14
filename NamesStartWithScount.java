21bccfc67bbe24ea591302407e99ad4bf2fb6a7a
dhanushyasubramanian <dhanushyasubramanian20@gmail.com>
2025-07-14 10:01:45 +0530

    NamesStartWithScount.java

---
+++ NamesStartWithScount.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NamesStartWithScount {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("pramisha");
+        names.add("dhanush");
+        names.add("dhanushya");
+        names.add("rishika");
+        names.add("shrika");
+        names.add("sri vignesh");
+        long nameCount = names.stream()
+        .filter(name -> name.startsWith("s")).count();
+        System.out.println(nameCount);
+
+    }
+}
+
